3d20c2ebd994195b4aa588aa4318a48eaf46a1da
“markelld” <markelldehaney@gmail.com>
2021-02-09 11:31:01 -0600

    backend almost completed

---
+++ helpers/restrict.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+const TOKEN_KEY = 'areallylonggoodkey'
+
+/// ask about token key // enviroment variable
+ const restrict = (req, res, next) => {
+
+        
+    try {
+      const token = req.headers.authorization.split(' ')[1]
+      const data = jwt.verify(token, TOKEN_KEY)
+      next()
+  } catch (error) {
+      console.log(error)
+      res.status(403).send('Unauthorized')
+  }
+}
+
+module.exports = restrict
