e0e83bc1aaa9143339b7e745f0f9a4c3d168e5cd
“markelld” <markelldehaney@gmail.com>
2021-02-09 13:01:49 -0600

    env

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const logger = require('morgan')
 const routes = require('./routes')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3002
 
 app.use(cors())
 app.use(bodyParser.json())
