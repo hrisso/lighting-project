d9a5e74d79f56a542688d76ae9616bb49203a219
SaudiR <saudirodriguez20@gmail.com>
2021-02-09 16:03:37 -0500

    layout rendered, started crud

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Layout from "./components/shared/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
-      
+      <Layout />
     </div>
   );
 }
---
+++ client/src/components/shared/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.css";
+
+const Footer = (props) => (
+  <footer>
+    <a href="https://github.com/hrisso/lighting-project" target="_blank" alt="Github">
+      <img src="https://res.cloudinary.com/doas1ztbf/image/upload/v1612902916/Octocat_kh8pkg.png"/>
+    </a>
+  </footer>
+)
+export default Footer;
