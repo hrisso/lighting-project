4727ca5217cd5615aefaa3edb6e46399d5acdc3d
hrisso <heather.risso@gmail.com>
2021-02-10 11:01:42 -0500

    product component works

---
+++ client/src/components/ProductCards/ProductCards.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ProductCards.css';
+import { Link } from 'react-router-dom'
+
+const ProductCards = (props) => {
+    return (
+        <>
+            <Link className="product-cards" to={`/products/${props._id}`}>
+                <img className="product-image" src={props.imgURL} alt={props.name} />
+                <div className="product-name">{props.name}</div>
+                <div className="price">{`${props.price}`}</div>
+            </Link>
+        </>
+    )
+}
+
+export default ProductCards
