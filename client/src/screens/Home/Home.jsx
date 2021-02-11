d05146b021594ebc0da7634265bfbb494ddb97c6
“markelld” <markelldehaney@gmail.com>
2021-02-11 15:02:01 -0600

    home screen

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -22,40 +22,74 @@ const Home = ({slides}) => {
   
   return (
     <div>
-      <Layout>   
-      <div className="slider"> 
-        <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/> 
-        <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
-        {SliderData.map((slide, index) => { 
-          return (
-            <div className={index === current ? 'slide active' : 'slide'}
-            key={index}
-            >
-            {index === current && (
-              <img src={slide.image} alt='travel image' className='image' />
-            )}
-            </div>
-          
-          );
-      })} 
-      </div>  
-      <h1 className="heading-1">Ways to Feature Lighting Solutions</h1>
-      <div className="solutions-block"> 
-          
-        <div className="column">
-          <h2>Clear an area around your light</h2>  
-          <p>Having a clean space will help the features of the light pop in your decorating scheme. Keeping the area clear is also safer! Wires can get messy and be dangerous even if they are connected correctly. Have space around your light. 
-          </p>
-        </div> 
-        <div className="column">
-          <h2>Coordinate colors</h2>  
-          <p>Coordinate the colors of your light to complement the surrounding area or vice versa. You can either choose the light to match the decor or choose the lighting solutions first and fill in with furniture. Coordinating the color scheme will help your light ‘pop’. </p>
+      <Layout>
+        <div className="slider">
+          <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+          <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
+          {SliderData.map((slide, index) => {
+            return (
+              <div
+                className={index === current ? "slide active" : "slide"}
+                key={index}
+              >
+                {index === current && (
+                  <img src={slide.image} alt="travel image" className="image" />
+                )}
+              </div>
+            );
+          })}
         </div>
-        <div className="column">
-          <h2>Choose the right size</h2>  
-          <p>Sizing your light is important. Too large or too small and the light looks ridiculous. Remember the rule of threes or use the golden ratio to measure and buy a correctly sized light.</p>
-        </div>  
-        </div> 
+        <h1 className="heading-1">Ways to Feature Lighting Solutions</h1>
+        <div className="solutions-block">
+          <div className="column">
+            <div className="heading">
+              <img
+                className="icon"
+                alt="icon"
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.flaticon.com%2Ficons%2Fpng%2F512%2F4205%2F4205363.png&f=1&nofb=1"
+              />
+              <h2>Clear an area around your light</h2>
+            </div>
+            <p>
+              Having a clean space will help the features of the light pop in
+              your decorating scheme. Keeping the area clear is also safer!
+              Wires can get messy and be dangerous even if they are connected
+              correctly. Have space around your light.
+            </p>
+          </div>
+          <div className="column"> 
+            <div className="heading">
+            <img
+                className="icon"
+                alt="icon"
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.flaticon.com%2Ficons%2Fpng%2F512%2F4205%2F4205363.png&f=1&nofb=1"
+              />
+              <h2>Coordinate colors</h2>
+            </div>
+            <p>
+              Coordinate the colors of your light to complement the surrounding
+              area or vice versa. You can either choose the light to match the
+              decor or choose the lighting solutions first and fill in with
+              furniture. Coordinating the color scheme will help your light
+              ‘pop’.{" "}
+            </p>
+          </div>
+          <div className="column"> 
+            <div className="heading">
+              <img
+                  className="icon"
+                  alt="icon"
+                  src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.flaticon.com%2Ficons%2Fpng%2F512%2F4205%2F4205363.png&f=1&nofb=1"
+                />
+              <h2>Choose the right size</h2>
+            </div>
+            <p>
+              Sizing your light is important. Too large or too small and the
+              light looks ridiculous. Remember the rule of threes or use the
+              golden ratio to measure and buy a correctly sized light.
+            </p>
+          </div>
+        </div>
       </Layout>
     </div>
   );
@@ -63,6 +97,6 @@ const Home = ({slides}) => {
 
 export default Home;
 
-     
+
       
     
